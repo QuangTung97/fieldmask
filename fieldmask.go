95a6f2183fc0fe0d92b8d79d82874f790ba338a1
tung.tq <tung.tq@teko.vn>
2023-06-20 16:06:21 +0700

    Add Generated Example

--- fieldmask.go
+++ fieldmask.go
@@ -1,1 +1,11 @@
 package fieldmask
+
+// FieldInfo ...
+type FieldInfo struct {
+	JsonName  string
+	SubFields []FieldInfo
+}
+
+func ComputeFieldInfos(fields []string) ([]FieldInfo, error) {
+	return nil, nil
+}
